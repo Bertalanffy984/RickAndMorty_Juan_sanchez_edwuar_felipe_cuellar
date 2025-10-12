952f6a0c6ca8c2af00fc74eccf72e8824936524d
Bertalanffy984 <szjuanda@outlook.com>
2025-10-12 18:17:23 -0500

    entregaFinal

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/ApiSimpsons_2025IIg1_Juan_Sanchez_Edwuar_Cuellar",
   plugins: [
     react(),
     tailwindcss()
